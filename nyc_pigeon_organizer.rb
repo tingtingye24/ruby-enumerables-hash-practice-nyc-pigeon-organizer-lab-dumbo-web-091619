6f0a45de9809cde7964b165ddd2c6598965c05cf
Ting Ting Ye <tingtingye24@gmail.com>
2019-08-20 00:55:14 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,4 +1,7 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
-  
+  data.each do |key,value|
+    value.each do |secondKey, name|
+      name.each do |name|
+        if data[name]!
 end
