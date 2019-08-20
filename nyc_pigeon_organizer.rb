b51d5400935cb495726f9958c872244452d12982
Ting Ting Ye <tingtingye24@gmail.com>
2019-08-20 00:56:51 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,12 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  newhash = {}
   data.each do |key,value|
     value.each do |secondKey, name|
       name.each do |name|
-        if data[name]!
+        if !newhash[name]
+          newhash[name] => {}
+        end
+        if !newhash[secondKey]
+          newhash
 end
